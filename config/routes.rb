40038bc1c5cc95adb80a1d5a299789d7992e03ce
msilvaj <miqueias.ufpi@gmail.com>
2021-03-13 17:50:06 -0300

    Add model Project.rb and configure create and javascript

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'project/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get 'project/index'
+  get 'projects', to: 'project#index'
+  post 'projects', to: 'project#create'
 end
