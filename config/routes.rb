59034f3fd6e2162e1a61edaa959026053f9cde5d
msilvaj <miqueias.ufpi@gmail.com>
2021-03-31 15:03:31 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   #get 'project/index'
   get 'projects', to: 'project#index'
+  get 'projects/:id', to: 'project#show' , as: 'project'
   post 'projects', to: 'project#create'
 end
